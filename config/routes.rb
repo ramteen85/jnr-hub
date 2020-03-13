37e8b5709712497cda57e8778fcc61629388743c
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-03-13 19:32:41 +1100

    readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # post 'user_token' => 'user_token#create'
-  post '/tokens' => 'tokens#create'
   resources :tests
   root 'users#home'
   post '/users/register' => 'users#register'
